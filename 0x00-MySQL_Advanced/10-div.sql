55c3855b37966b3645fce524ae13ee577f409c60
mrxdoffical <berogergesg@gmail.com>
2024-10-18 20:36:00 +0300

    task10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- Create the SafeDiv function to safely divide two numbers
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
